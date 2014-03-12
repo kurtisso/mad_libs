fa57fb1875cecff61c2ee482b5e9b933bbe960c9
Kurtis So <kso@lsoc.org>
2014-03-12 18:44:15 +0000

    mad_libs First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MadLibs::Application.config.secret_key_base = '7ea6ad7b94b1c23b65cae7bd654743b4049a6487b1792986f1085a2b323f2ea7c68a3d6495f930f3daefc8e0559fa7b764d0c2ccffc045b50474cf317a488500'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MadLibs::Application.config.session_store :cookie_store, key: '_mad_libs_session'
